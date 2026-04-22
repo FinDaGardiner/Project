afca67fe677473e9f90f1611de651484f419a367
FinDaGardiner <157618651+FinDaGardiner@users.noreply.github.com>
2026-04-22 13:09:16 +0100

    final changes + images

--- scripts/example-map.js
+++ scripts/example-map.js
@@ -1,41 +1,41 @@
 initTour({
   id: "hills",
-  arrivalRadius: 40,
+  arrivalRadius: 20,
   routeRedrawThreshold: 15,
   initialZoom: 13,
   userCenterZoom: 15,
   defaultCenter: [55.9533, -3.1883],
   accuracyBuffer: 15,
-  maxEffectiveArrivalRadius: 120,
+  maxEffectiveArrivalRadius: 60,
   points: [
     {
       name: "Tesco",
       coords: [55.934056, -3.210611],
-      image: "images/arthurs-seat.jpg",
+      image: "images/tesco.jpg",
       info: "The closest Supermarket to campus, Tesco is a staple for the University and is rammed at lunch time with everyone piling in for Meal Deals"
     },
     {
       name: "Voi Bike Stop",
       coords: [55.933444, -3.212389],
-      image: "images/edinburgh-castle.jpg",
+      image: "images/voibike.jpg",
       info: "Easily pick up and drop off a bike from the Voi bike stop, download their app to do so"
     },
     {
       name: "Film & Media building",
       coords: [55.934194, -3.211611],
-      image: "images/calton-hill.jpg",
+      image: "images/filmbuilding.jpg",
       info: "This is the Film & Media building, I don't know what goes on in here but it looks cool"
     },
     {
       name: "JKCC",
       coords: [55.933079, -3.214402],
-      image: "images/corstorphine-hill.jpg",
+      image: "images/jkcc.jpg",
       info: "The Jack Kilby Computing Centre (JKCC) is the greatest place on earth, built in 2001, Jack Kilby, born in America, invented the first integrated circuit (microchip) in 1958, fundamentally enabling modern electronics and earning him the 2000 Nobel Prize in Physics."
     },
     {
       name: "Library",
       coords: [55.933376, -3.213328],
-      image: "images/craiglockhart-hill.jpg",
+      image: "images/library.jpg",
       info: "This is the library, a place for students who prefer a quieter environment to study, as well as read and rent books"
     },
 
